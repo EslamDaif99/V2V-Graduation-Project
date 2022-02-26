edff40b461d5d6991d1336786c5bec98a3dde723
MariamSaidGaber <saidmariam463@gmail.com>
2022-02-26 19:54:58 +0200

    blink_led

--- src/03_MCAL/Timer/Timer.c
+++ src/03_MCAL/Timer/Timer.c
@@ -11,9 +11,10 @@ static void inline enable_irq(void)
 void Timer_Init(void)
 {
     /* Configure SysTick */
+    NVIC_ST_CTRL_R =0;              /* Disable timer*/
     NVIC_ST_RELOAD_R = 16000000-1;  /* reload with number of clocks per second */
     NVIC_ST_CTRL_R = 7;             /* enable SysTick interrupt, use system clock */
-
+    NVIC_ST_CURRENT_R	=0;	    /*Clear Current value*/ 
     enable_irq();                   /* global enable interrupt */
 }
 
