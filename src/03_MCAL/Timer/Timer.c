6499c21510206df3c0769651739e3c40069b9dbd
MariamSaidGaber <saidmariam463@gmail.com>
2022-02-26 15:40:54 +0200

    modift_2

--- src/03_MCAL/Timer/Timer.c
+++ src/03_MCAL/Timer/Timer.c
@@ -1,13 +1,27 @@
 #include "Timer.h"
 #include "OS.h"
+#include "tm4c123gh6pm.h"
 
+static void inline enable_irq(void)
+{
+    __asm  ("    CPSIE  I\n");
+}
 
 void Timer_Init(void)
 {
-    /* init systick timer */
+    /* Configure SysTick */
+    NVIC_ST_RELOAD_R = 16000000-1;  /* reload with number of clocks per second */
+    NVIC_ST_CTRL_R = 7;             /* enable SysTick interrupt, use system clock */
+
+    enable_irq();                   /* global enable interrupt */
 }
 
-void Timer_Update(void) /* ISR every 1 second */ 
+void SysTick_Handler(void)
+{
+    Timer_Update();
+}
+
+void Timer_Update(void) /* ISR every 1 second */
 {
     OS_IncreamentTick();
 }
