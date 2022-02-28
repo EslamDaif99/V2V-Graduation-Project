209340c605e5947b902bd9674543a1367a7241fe
MariamSaidGaber <saidmariam463@gmail.com>
2022-02-28 15:59:26 +0200

    LED

--- src/Port.h
+++ src/Port.h
@@ -5,38 +5,6 @@
 #define LED_PIN_NUMBER     (GPIO_PIN_0)
 
 
-#define MOTOR_1_PORT       (GPIO_PORTB)
-#define MOTOR_2_PORT       (GPIO_PORTB)
-#define MOTOR_3_PORT       (GPIO_PORTD)
-#define MOTOR_4_PORT       (GPIO_PORTD)
-
-
-/***************Motor 1***************/
-#define MOTOR_1_ENABLE     (GPIO_PIN_4)       /*PB4 PWM */
-#define MOTOR_1_IN1        (GPIO_PIN_0)
-#define MOTOR_1_IN2        (GPIO_PIN_1)
-
-/***************Motor 2***************/
-#define MOTOR_2_ENABLE     (GPIO_PIN_5)       /*PB5 PWM */
-#define MOTOR_2_IN1        (GPIO_PIN_2)
-#define MOTOR_2_IN2        (GPIO_PIN_3)
-
-/***************Motor 3***************/
-#define MOTOR_3_ENABLE     (GPIO_PIN_0)	      /*PD0 PWM */
-#define MOTOR_3_IN1        (GPIO_PIN_2)
-#define MOTOR_3_IN2        (GPIO_PIN_3)
-
-/***************Motor 4***************/
-#define MOTOR_4_ENABLE     (GPIO_PIN_1)	     /*PD1 PWM */
-#define MOTOR_4_IN1        (GPIO_PIN_4)
-#define MOTOR_4_IN2        (GPIO_PIN_5)
-
-
-
-
-
-
-
 
 
 
