8e0bb6961ec6bc1a9ba2cf2436c7210db3e76835
Ahmed Galal <ahmed.galal@swift-act.com>
2022-03-09 17:34:33 +0200

    add motor arch

---
+++ src/02_HAL/Motor/Motor.c
@@ -0,0 +1,27 @@
+#include "Motor.h"
+
+void Motor_Init(void)
+{
+
+}
+
+void Motor_Update(void)
+{
+
+}
+void Motor_Start(void)
+{
+
+}
+void Motor_Stop(void)
+{
+
+}
+void Motor_GoForward(void)
+{
+
+}
+void Motor_SetSpeed(Motor_Speed_t speed)
+{
+    
+}
