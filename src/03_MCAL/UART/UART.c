6fb75568520f1b93ed406097861d4a570c63de4d
shahd8796 <100058176+shahd8796@users.noreply.github.com>
2022-03-23 20:08:27 +0200

    Add files via upload

--- src/03_MCAL/UART/UART.c
+++ src/03_MCAL/UART/UART.c
@@ -27,32 +27,32 @@ void UART_Init(void)
 	SET_BIT(GPIO_PORTB_DIR_R,1);
 	SET_BIT(GPIO_PORTB_DEN_R,1);
 	/*Disable UART*/
-	CLR_BIT(UART0_CTL_R,0);
+	CLR_BIT(UART1_CTL_R,0);
 	/*Baud rate*/
-	UART0_IBRD_R =8;    //16000000/(16*115200) =8.6805;
-	UART0_FBRD_R =44;    //.6805*64+.5=44.05
+	UART1_IBRD_R =8;    //16000000/(16*115200) =8.6805;
+	UART1_FBRD_R =44;    //.6805*64+.5=44.05
 	/*1 STOP bit*/
-	CLR_BIT(UART0_LCRH_R,3);
+	CLR_BIT(UART1_LCRH_R,3);
 	/*8 bit data*/
-	SET_BIT(UART0_LCRH_R,5);
-	SET_BIT(UART0_LCRH_R,6);
+	SET_BIT(UART1_LCRH_R,5);
+	SET_BIT(UART1_LCRH_R,6);
 	/*Enable UART*/
-	SET_BIT(UART0_CTL_R,0);
+	SET_BIT(UART1_CTL_R,0);
 
 }
 
 void UART_Tx(uint08_t byte)
 {
 	/*Wait for TXFF*/
-	while(GET_BIT(UART0_FR_R,5)==1){};
-	UART0_DR_R = byte;
+	while(GET_BIT(UART1_FR_R,5)==1){};
+	UART1_DR_R = byte;
 }
 
 uint08_t UART_Rx(uint08_t * bytePtr)
 {
 	/*Wait for RXFE*/
-	while(GET_BIT(UART0_FR_R,4)==0){};
-	* bytePtr = UART0_DR_R;
+	while(GET_BIT(UART1_FR_R,4)==0){};
+	* bytePtr = UART1_DR_R;
     return UART_RX_UNAVAILABLE;
 }
 
