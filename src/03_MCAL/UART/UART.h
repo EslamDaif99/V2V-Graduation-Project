c94a65db1adb224f75d69e67f4e9f1a43bdf37ac
shahd8796 <100058176+shahd8796@users.noreply.github.com>
2022-05-06 00:45:23 +0200

    Add files via upload

--- src/03_MCAL/UART/UART.h
+++ src/03_MCAL/UART/UART.h
@@ -4,9 +4,22 @@
 #define UART_RX_AVAILABLE           (0xAB)
 #define UART_RX_UNAVAILABLE         (0xCD)
 
-void UART_Init(void);
-void UART_Tx(uint08_t byte);
-uint08_t UART_Rx(uint08_t * bytePtr);
+typedef enum
+{
+	/* List the UART*/
+	UART_0,
+	UART_1,
+	UART_2,
+	UART_3,
+	UART_4,
+	UART_5,
+	UART_6,
+	UART_7
+
+}UART_t;
+void UART_Init(uint08_t  UART_t);
+void UART_Tx(uint08_t byte,uint08_t  UART_t);
+uint08_t UART_Rx(uint08_t * bytePtr , uint08_t  UART_t);
 void UART_Update(void);
 
 #endif
