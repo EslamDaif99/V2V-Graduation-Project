019adb9155cdc22483e7868e18d61c6792beacc2
shahd8796 <100058176+shahd8796@users.noreply.github.com>
2022-05-08 01:58:35 +0200

    Add files via upload

---
+++ 02_HAL/Speed/Speed.c
@@ -0,0 +1,22 @@
+#include "Type.h"
+#include "BITMATH.h"
+#include "INTPin.h"
+#include "Speed.h"
+#include "GPIO.h"
+#include "tm4c123gh6pm.h"
+
+
+void SpeedInit(void)
+{
+	SpeedPinConfig(SpeedAction);
+	EnableSpeedInt();
+}
+void SpeedAction(void)
+{
+	Counts++;
+	/*For check*/
+	GPIO_SetPinDirection(GPIO_PORTD,GPIO_PIN_1,GPIO_DIRECTION_OUTPUT);
+	GPIO_SetPinState(GPIO_PORTD,GPIO_PIN_1,GPIO_STATE_HIGH);
+	/*Clear Flag*/
+	SET_BIT(GPIO_PORTD_ICR_R,0);
+}
--- 02_HAL/Speed/Speed.h
+++ 02_HAL/Speed/Speed.h
@@ -1,1 +1,11 @@
+#ifndef SPEED_H_
+#define SPEED_H_
 
+
+#include"TM4C123GH6PM.h"
+
+void SpeedInit(void);
+void SpeedAction(void);
+
+
+#endif /* SPEED_H_ */
