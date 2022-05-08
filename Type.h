fe0f520a7b2ff24c8762f6d4a3c80f0e20ea5f09
Eslam Ali <92636992+EslamDaif99@users.noreply.github.com>
2022-05-08 10:54:43 +0200

    Add files via upload

--- Type.h
+++ Type.h
@@ -1,10 +1,18 @@
 #ifndef _TYPE_H_
 #define _TYPE_H_
 
-typedef unsigned char uint08_t;
-typedef unsigned short int uint16_t;
-typedef unsigned long int uint32_t;
-typedef unsigned long long uint64_t;
+typedef unsigned char         uint08_t;             /*           0 .. 255              */
+typedef signed char           sint08_t;             /*        -128 .. +127             */
+typedef unsigned short int    uint16_t;             /*           0 .. 65535            */
+typedef signed short          sint16_t;             /*      -32768 .. +32767           */
+typedef unsigned long int     uint32_t;             /*           0 .. 4294967295       */
+typedef unsigned long long    uint64_t;
+typedef signed long long      sint64_t;
+typedef float                 float32_t;
+typedef double                float64_t;
+
+
+
 
 #define NULL_PTR			((void *)(0))
 
